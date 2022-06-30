225e547a0da1bf94b333fee317408d705779773e
Annicca <101399976+Annicca@users.noreply.github.com>
2022-06-30 16:44:54 +0300

    body_media

--- Gulp-Dimohod/src/components/header/header.js
+++ Gulp-Dimohod/src/components/header/header.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('option_button');
 btn.addEventListener('click', () => {
   const menu = document.getElementById('list_menu');
+  const burger = document.getElementById('burger');
   menu.classList.toggle('header_list_open');
+  burger.classList.toggle('option_burger_open');
 });
