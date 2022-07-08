1db893eab2db1cb34d5a3303bb9fbd1f8bad1ae8
Annicca <101399976+Annicca@users.noreply.github.com>
2022-07-08 14:59:28 +0300

    правки

--- Gulp-Dimohod/src/components/header/header.js
+++ Gulp-Dimohod/src/components/header/header.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const btn = document.getElementById('option_button');
 btn.addEventListener('click', () => {
   const menu = document.getElementById('list_menu');
@@ -6,13 +5,3 @@ btn.addEventListener('click', () => {
   menu.classList.toggle('header_list_open');
   burger.classList.toggle('option_burger_open');
 });
-=======
-const btn = document.getElementById('option_burger_button');
-if (btn) {
-  btn.style.backgroundColor = 'red';
-  btn.addEventListener('click', () => {
-    const menu = document.getElementById('menu_list');
-    menu.classList.toggle('menu_list--open');
-  });
-}
->>>>>>> Stashed changes
